15f549c5c9d51946ca9c466a8d872aa596222ec7
gumball898 <arnavshuban@gmail.com>
2026-06-22 22:41:10 -0700

    please be there for the love of god in git

---
+++ data types.py
@@ -0,0 +1,16 @@
+# 1) Store an integer value in `a`.
+print(a = 90000)
+# 2) Print the datatype of `a` using `type()`.
+print(type (a))
+# 3) Store a decimal (float) value in `b`.
+print(store = 3.14)
+# 4) Print the datatype of `b` using `type()`.
+print(type (store))
+# 5) Store a text (string) value in `c`.
+print(c = "hellow world")
+# 6) Print the datatype of `c` using `type()`.
+print(type (c))
+# 7) Store a boolean value (True/False) in `d`.
+print(d = True)
+# 8) Print the datatype of `d` using `type()`.
+print(type(d))
