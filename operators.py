4e64df26627134b5e677fba238933892e2be2773
gumball898 <arnavshuban@gmail.com>
2026-06-26 16:50:21 -0700

    please be on git

---
+++ operators.py
@@ -0,0 +1,19 @@
+math=2
+science=5
+computer_science=math**science
+print(computer_science)
+
+# 1) Store the values for each tree in separate variables: tree1, tree2, tree3, tree4, tree5
+tree1 = 10
+tree2 = 15
+tree3 = 20
+tree4 = 25
+tree5 = 30
+# 2) Add all 5 tree values and store the result in `sum`
+trees=tree1 + tree2 + tree3 + tree4 + tree5
+# 3) Print the total (`sum`) of all 5 trees
+print(trees)
+# 4) Divide `sum` by 5 to find the average and store it in `average`
+i=trees/5
+# 5) Print the average (`average`) of all 5 trees
+print(i)
